060fd986a9c2f15d61f9fef4c69d877116638e16
Vinicius Dill Da Luz <vinidill9@gmail.com>
2024-08-30 08:20:56 -0300

    feat: create home page

--- src/Pages/Home/styles.ts
+++ src/Pages/Home/styles.ts
@@ -1,3 +1,46 @@
 import styled from "styled-components";
 
-export const Container = styled.div``
+export const Container = styled.div`
+  box-sizing: border-box;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+  background-image: url('./icons/fundo.jpeg');
+  background-size: cover;
+  background-position: center;
+  text-align: center;
+
+  position: relative;
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 0;
+  }
+
+  h1, p {
+    z-index: 1;
+    position: relative;
+  }
+
+  h1 {
+    color: #FFC506;
+    font-size: 64px;
+    font-weight: bold;
+  }
+
+  p {
+    color: #A6A6A6;
+    font-size: 36px;
+    font-weight: bold;
+  }
+`;
--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "../Pages/Login";
 import Home from "../Pages/Home";
+import Tests from "../Pages/Tests";
 
 function AppRoutes() {
   return (
@@ -8,7 +9,8 @@ function AppRoutes() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
-        <Route path="*" element={<div>404 Not Found</div>} />{" "}
+        <Route path="/tests" element={<Tests />} />
+        <Route path="*" element={<div>404 Not Found</div>} />
         {/* Página de erro 404 */}
       </Routes>
     </BrowserRouter>
